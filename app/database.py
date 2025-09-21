2e579e85ffbff1ad8438a47791d8f34a03fff726
SirOtaiv <rauotavio7@gmail.com>
2025-09-21 20:48:51 -0300

    Fix: Update MongoDB connection string to include database and authSource

--- app/database.py
+++ app/database.py
@@ -2,6 +2,6 @@ from motor.motor_asyncio import AsyncIOMotorClient
 from app.config import settings
 
 database_client = AsyncIOMotorClient(
-    f"mongodb://{settings.mongodb_username}:{settings.mongodb_password}@{settings.mongodb_host}:{settings.mongodb_port}"
+    f"mongodb://{settings.mongodb_username}:{settings.mongodb_password}@{settings.mongodb_host}:{settings.mongodb_port}/{settings.mongodb_database}?authSource={settings.mongodb_database}"
 )
-mongo_database_con = database_client[settings.mongodb_database]
+mongo_database_con = database_client
