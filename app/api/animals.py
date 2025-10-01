4c9e623a8f40f04355205b382e4de767c6819321
SirOtaiv <rauotavio7@gmail.com>
2025-10-01 01:19:43 -0300

    Criado API de Animals, método POST / Corrigido type de response

--- app/api/animals.py
+++ app/api/animals.py
@@ -1,10 +1,9 @@
 from fastapi import APIRouter
-from Backend.app.models.animals import Animal
-from Backend.app.schemas.animals import AnimalCreate
+from Backend.app.schemas.animals import AnimalCreate, AnimalResponse
 
 router = APIRouter()
 
-@router.post("/animals", response_model=Animal)
+@router.post("/animals", response_model=AnimalResponse)
 async def create_animal(animal: AnimalCreate):
     # lógica de salvar no Mongo
     new_animal = {**animal.dict(), "id": "generated_mongo_id"}
