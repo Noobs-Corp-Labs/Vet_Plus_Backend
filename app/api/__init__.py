061cf1ddf6613205933688e64f155db11868114a
SirOtaiv <rauotavio7@gmail.com>
2025-10-01 01:21:56 -0300

    Incluido rota na API

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter
-from app.api import users, admin, health
+from app.api import animals, users, admin, health
 
 api_router = APIRouter()
 
 api_router.include_router(admin.router, prefix='/admin', tags=['Admin'])
-api_router.include_router(health.router, prefix='/health', tags=['Health'])
+api_router.include_router(animals.router, prefix='/animals', tags=['Animals'])
 api_router.include_router(users.router, prefix='/users', tags=['Users'])
+api_router.include_router(health.router, prefix='/health', tags=['Health'])
