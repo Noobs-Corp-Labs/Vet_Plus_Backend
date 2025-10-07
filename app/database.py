39d28c4beaa64c316b12b34c9c86d93c357f96de
SirOtaiv <rauotavio7@gmail.com>
2025-10-06 21:03:01 -0300

    Remove unused import and print statement from database.py

--- app/database.py
+++ app/database.py
@@ -1,9 +1,7 @@
 from motor.motor_asyncio import AsyncIOMotorClient
 from app.config import settings
 import urllib.parse
-from pymongo.errors import ServerSelectionTimeoutError
 
 MONGO_URI = f"mongodb://{settings.mongodb_username}:{urllib.parse.quote_plus(settings.mongodb_password)}@{settings.mongodb_host}:{settings.mongodb_port}/?authSource={settings.mongodb_database}"
-print("MONGO:", MONGO_URI)
 database_client = AsyncIOMotorClient(MONGO_URI)
 mongo_database_con = database_client[settings.mongodb_database]
