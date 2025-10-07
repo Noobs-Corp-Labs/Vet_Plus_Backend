3762ce33a7bab9877d537cd0bde0922528b82b4c
SirOtaiv <rauotavio7@gmail.com>
2025-10-06 21:06:14 -0300

    Remove unnecessary urllib.parse import and simplify MONGO_URI construction

--- app/database.py
+++ app/database.py
@@ -1,7 +1,6 @@
 from motor.motor_asyncio import AsyncIOMotorClient
 from app.config import settings
-import urllib.parse
 
-MONGO_URI = f"mongodb://{settings.mongodb_username}:{urllib.parse.quote_plus(settings.mongodb_password)}@{settings.mongodb_host}:{settings.mongodb_port}/?authSource={settings.mongodb_database}"
+MONGO_URI = f"mongodb://{settings.mongodb_username}:{settings.mongodb_password}@{settings.mongodb_host}:{settings.mongodb_port}/?authSource={settings.mongodb_database}"
 database_client = AsyncIOMotorClient(MONGO_URI)
 mongo_database_con = database_client[settings.mongodb_database]
