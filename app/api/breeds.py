78f985261883bdf539b1b89cd992d0f40c65ffdf
SirOtaiv <rauotavio7@gmail.com>
2025-10-21 20:48:42 -0300

    Add endpoint to retrieve a single breed and handle not found error

--- app/api/breeds.py
+++ app/api/breeds.py
@@ -1,8 +1,9 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Path, status
+from typing import Annotated
 
 from app.schemas.communs import ListResponse
 from app.schemas.breeds import BreedResponse
-from app.crud.breeds import find_all_breeds
+from app.crud.breeds import find_all_breeds, find_one_breed
 
 
 router = APIRouter()
@@ -26,3 +27,22 @@ async def get_all_breeds():
             status_code=500,
             detail="Erro interno ao buscar raças."
         )
+
+@router.get("/{breed_identifier}",
+    response_model=BreedResponse,
+    status_code=status.HTTP_200_OK,
+)
+async def get_one_breed(
+    breed_identifier: Annotated[
+        str,
+        Path(
+            description="Name or Id of the breed to retrieve",
+            example="String"
+        )
+    ]
+):
+    breed_obj = await find_one_breed(breed_identifier)
+    if not breed_obj:
+        raise HTTPException(status_code=404, detail='Breed not found')
+    breed_obj["_id"] = str(breed_obj["_id"])
+    return breed_obj
--- app/crud/breeds.py
+++ app/crud/breeds.py
@@ -1,6 +1,17 @@
+from bson import ObjectId
+from bson.errors import InvalidId
 from app.database import mongo_database_con
 
 async def find_all_breeds():
-    breed = await mongo_database_con["breeds"].find().to_list(length=None)
-    return breed
+    breeds = await mongo_database_con["breeds"].find().to_list(length=None)
+    return breeds
     
+async def find_one_breed(breed_identifier: str):
+    filter_query = {"$or": [{"name": breed_identifier}]}
+    try:
+        filter_query["$or"].append({"_id": ObjectId(breed_identifier)})
+    except (InvalidId, Exception):
+        pass
+    
+    breed = await mongo_database_con["breeds"].find_one(filter=filter_query)
+    return breed
