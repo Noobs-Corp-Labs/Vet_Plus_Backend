2fed161b591ba140201311ca2243df8b584a40fa
SirOtaiv <rauotavio7@gmail.com>
2025-11-07 23:12:04 -0300

    feat: Atualizado script build & deploy / Adicionado config env para build

--- app/config.py
+++ app/config.py
@@ -6,20 +6,20 @@ class Settings(BaseSettings):
     swagger_servers_list: Optional[str] = None
 
     # Server
-    secret_key: str
-    algorithm: str
-    access_token_expire_minutes: int
+    secret_key: str = "devsecret"
+    algorithm: str = "HS256"
+    access_token_expire_minutes: int = 30
 
     # MongoDB
-    mongodb_host: str
-    mongodb_port: int
-    mongodb_username: str
-    mongodb_password: str
-    mongodb_database: str
+    mongodb_host: str = "localhost"
+    mongodb_port: int = 27017
+    mongodb_username: str = "root"
+    mongodb_password: str = "root"
+    mongodb_database: str = "vetplus"
 
     # Gemini
-    gemini_api_key: str
-    gemini_model: str
+    gemini_api_key: str = "fake"
+    gemini_model: str = "gemini-test"
 
     class Config:
         env_file = ".env"
